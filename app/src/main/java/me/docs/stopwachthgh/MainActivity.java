4a8ee82306b8ea7de20e065c2fd01346ac9ce05a
kitykatgirl <kamilord220@vp.pl>
2026-03-31 08:15:02 +0200

    basically errything xcept saving gruhh i love seesay

--- app/src/main/java/me/docs/stopwachthgh/MainActivity.java
+++ app/src/main/java/me/docs/stopwachthgh/MainActivity.java
@@ -2,6 +2,8 @@ package me.docs.stopwachthgh;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     private int sekundy = 0;
 
     private TextView textView;
+    private Button startButton;
+    private Button stopButton;
+    private Button resetButton;
+    private Button zapiszButton;
+
+    private boolean czyDziala = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +35,48 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         textView = findViewById(R.id.textViewCzas);
+        startButton = findViewById(R.id.btnStart);
+        stopButton = findViewById(R.id.btnStop);
+        resetButton = findViewById(R.id.btnReset);
+        zapiszButton = findViewById(R.id.btnZapisz);
 
         Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                sekundy++;
-                textView.setText(String.valueOf(sekundy));
+                if(czyDziala){
+                    sekundy++;
+                    textView.setText(wyswietlCzas(sekundy));
+                }
                 handler.postDelayed(this,1000);
             }
         });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                czyDziala = true;
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                czyDziala = false;
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                czyDziala = false;
+                sekundy = 0;
+                textView.setText(wyswietlCzas(0));
+            }
+        });
+    }
+    private String wyswietlCzas(int ile){
+        int sekundy = ile%60;
+        int minuty = (ile/60)%60;
+        int godziny = ile/3600;
+        return String.format("%02d:%02d:%02d",godziny,minuty,sekundy);
     }
 }
